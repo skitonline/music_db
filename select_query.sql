6521a2a6f6bc82dc95c5afb674b3b9d8073bf606
skitonline <skitonline@yandex.ru>
2021-06-08 14:02:43 +0400

    add select query

---
+++ select_query.sql
@@ -0,0 +1,11 @@
+SELECT name, year FROM album
+	WHERE year = 2018;
+SELECT name, duration FROM music
+	ORDER BY duration DESC
+	LIMIT 1;
+SELECT name FROM collection
+	WHERE year BETWEEN 2018 AND 2020;
+SELECT name FROM musician 
+	WHERE name NOT LIKE '%_% %_%';
+SELECT name FROM music
+	WHERE name ILIKE '%my%' OR name ILIKE '%мой%';
